c52f2aba7115da3c6fbde877faebecc597bef9b8
Bo Granlund <bo.granlund@gmail.com>
2015-05-13 14:53:37 +0300

    Added some code

--- src/main/java/rssfr/rssfeedreader/Hello.java
+++ src/main/java/rssfr/rssfeedreader/Hello.java
@@ -12,5 +12,6 @@ package rssfr.rssfeedreader;
 public class Hello {
     public static void main(String [] args) {
         System.out.println("Hello World!\n");
+	System.out.println("Another Hello World!\n");
     }
 }
